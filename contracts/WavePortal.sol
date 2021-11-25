2c6db00339efdb4df376a2dc4f1701697a1ee012
Adebayo Samuel <eladebayoor@gmail.com>
2021-11-25 01:50:34 +0100

    I figured out how to count the frequency of elements in an array. I had to learn struct and mapping. A bit tricky tbh

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,11 +1,39 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract WavePortal {
-    constructor() {
-        console.log("My first console.log in solidity :) ");
+    uint256 totalWaves;
+    address[] persons;
+    struct Count{
+        uint timesWaved;
     }
+
+    mapping (address => Count) counts;
+
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        persons.push(msg.sender);
+        for (uint i=0; i < persons.length; i++) {
+            if (counts[persons[i]].timesWaved > 0) { 
+                counts[persons[i]].timesWaved += 1;
+            } else counts[persons[i]].timesWaved = 1;  
+        }
+        console.log("%s has waved %s times", msg.sender, counts[msg.sender].timesWaved);
+        for (uint i=0; i < persons.length; i++) {
+            counts[persons[i]].timesWaved = 0; 
+        }              
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
 }
